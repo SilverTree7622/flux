f3cdd82c858eba6be332d18248d53bf4e1b45823
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2024-08-09 12:50:37 +0900

    [feature] deploy

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    build: {
-        ssr: false
-    },
+    ssr: false,
     modules: [
         '@pinia/nuxt',
         '@nuxtjs/tailwindcss',
