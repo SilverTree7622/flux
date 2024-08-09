cd9355833d0d31452bdc62c1285cca3f5268cf91
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2024-08-09 12:49:41 +0900

    Updates

---
+++ _nuxt/D4Xob5lh.js
@@ -0,0 +1,1 @@
+import{q as e,j as a,F as n,U as o,c as t,o as s}from"./YQf5GCCu.js";const p=e({__name:"index",setup(c){return a(async()=>{await n(),o("/fluxwork")}),(r,i)=>(s(),t("div"))}});export{p as default};
