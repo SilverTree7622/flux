3e3558d8412bef0204ecfcdd1539e8640b3844c0
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2024-08-10 22:52:13 +0900

    [file] remove useless

--- components/Content/PlayableAD/index.vue
+++ components/Content/PlayableAD/index.vue
@@ -1,10 +1,14 @@
 <template>
-    <div>
-        
+    <div v-for="(item, idx) in props.list">
+        <ContentItem :item="item" />
     </div>
 </template>
-<script setup script="ts">
+<script setup lang="ts">
+import type { TContentItem } from '~/types/content';
 
+const props = defineProps<{
+    list: TContentItem[];
+}>();
 </script>
 <style scoped>
     
