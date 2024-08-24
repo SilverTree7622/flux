81491023403606e0bc6488085f5db559595c5b92
Tree&DB Hunter <40912698+SilverTree7622@users.noreply.github.com>
2024-08-24 16:56:04 +0900

    [style] add option to tsconfig

--- pages/showcase.vue
+++ pages/showcase.vue
@@ -1,48 +1,17 @@
 <template>
     <div class="w-full h-full">
-        <NuxtLayout 
-            name="tabcontent"
-            :tabIdx="opt.tabIdx"
-        >
+        <NuxtLayout name="tabcontent" :tabIdx="opt.tabIdx">
             <div class="py-2"></div>
-            <ContentWebsite
-                v-if="opt.tabIdx === 0"
-                :list="list.website"
-            />
-            <ContentWebGame
-                v-if="opt.tabIdx === 1"
-                :list="list.webgame"
-            />
-            <ContentPlayableAD
-                v-if="opt.tabIdx === 2"
-                :list="list.playablead"
-            />
-            <ContentEtc
-                v-if="opt.tabIdx === 3"
-                :list="list.etc"
-            />
+            <ContentWebsite v-if="opt.tabIdx === 0" :list="list.website" />
+            <ContentWebGame v-if="opt.tabIdx === 1" :list="list.webgame" />
+            <ContentPlayableAD v-if="opt.tabIdx === 2" :list="list.playablead" />
+            <ContentEtc v-if="opt.tabIdx === 3" :list="list.etc" />
             <div class="py-2"></div>
-            <NuxtLayout
-                name="template"
-                :tabIdx="opt.tabIdx"
-                :isExist="template.isExist"
-            >
-                <ContentWebsite
-                    v-if="opt.tabIdx === 0"
-                    :list="template.website"
-                />
-                <ContentWebGame
-                    v-if="opt.tabIdx === 1"
-                    :list="template.webgame"
-                />
-                <ContentPlayableAD
-                    v-if="opt.tabIdx === 2"
-                    :list="template.playablead"
-                />
-                <ContentEtc
-                    v-if="opt.tabIdx === 3"
-                    :list="template.etc"
-                />
+            <NuxtLayout name="template" :tabIdx="opt.tabIdx" :isExist="template.isExist">
+                <ContentWebsite v-if="opt.tabIdx === 0" :list="template.website" />
+                <ContentWebGame v-if="opt.tabIdx === 1" :list="template.webgame" />
+                <ContentPlayableAD v-if="opt.tabIdx === 2" :list="template.playablead" />
+                <ContentEtc v-if="opt.tabIdx === 3" :list="template.etc" />
             </NuxtLayout>
             <FooterInfo v-if="opt.isMyInfoExist" />
         </NuxtLayout>
@@ -53,16 +22,17 @@
 import type { TContentItem } from '@/types/content';
 
 const route = useRoute();
+
 const opt = reactive({
-    tabIdx: <number> Number(route.query['tab'] ?? 0),
-    isMyInfoExist: <boolean> !!(route.query['info'] ?? false),
+    tabIdx: <number>Number(route.query['tab'] ?? 0),
+    isMyInfoExist: <boolean>!!(route.query['info'] ?? false),
 });
 
 const list = reactive({
-    isExist: <boolean> false,
-    website: <TContentItem[]> [],
-    webgame: <TContentItem[]> [],
-    playablead: <TContentItem[]> [
+    isExist: <boolean>false,
+    website: <TContentItem[]>[],
+    webgame: <TContentItem[]>[],
+    playablead: <TContentItem[]>[
         {
             thumbnail: 'logo/playablead/sweetopia.png',
             title: 'SWEETOPIA',
@@ -70,15 +40,15 @@ const list = reactive({
             contentType: 'file'
         }
     ],
-    etc: <TContentItem[]> [],
+    etc: <TContentItem[]>[],
 });
 
 const template = reactive({
-    isExist: <boolean> false,
-    website: <TContentItem[]> [],
-    webgame: <TContentItem[]> [],
-    playablead: <TContentItem[]> [],
-    etc: <TContentItem[]> [],
+    isExist: <boolean>false,
+    website: <TContentItem[]>[],
+    webgame: <TContentItem[]>[],
+    playablead: <TContentItem[]>[],
+    etc: <TContentItem[]>[],
 });
 
 watch(
@@ -128,6 +98,4 @@ onMounted(async () => {
 });
 </script>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
