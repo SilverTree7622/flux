1d1a70acac8f5bcb5427a83e1cd507085c217822
FluxHunterPortable <dong5966@gmail.com>
2024-10-27 16:33:00 +0900

    [feature] item thumbnail style rounding

--- components/Content/Item/Thumbnail.vue
+++ components/Content/Item/Thumbnail.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="w-20 h-20 border-slate-950 border-spacing-10 rounded-xl">
-        <img :src="props.src" :alt="''" />
+        <img :src="props.src" :alt="''" class="rounded-xl" />
     </div>
 </template>
 <script setup lang="ts">
