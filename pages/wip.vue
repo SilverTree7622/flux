25150a26ab78d2246dffc58b78144d96a9a56abe
FluxHunterPortable <dong5966@gmail.com>
2025-03-10 03:31:20 +0900

    [file] wgames stuff

---
+++ pages/wip.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <iframe
+            ref="$frame"
+            class="w-full h-[100vh] overflow-hidden"
+            :src="opt.fullSrc"  
+        >
+        </iframe>
+    </div>
+</template>
+
+<script setup lang="ts">
+const route = useRoute();
+
+const opt = reactive({
+    fullSrc: '',
+});
+
+onMounted(async () => {
+    await nextTick();
+    const company = route.query['tab'] as string ?? '';
+    const game = route.query['game'] as string ?? '';
+    opt.fullSrc = `${ company }/${ game }/index.html`;
+});
+</script>
+
+<style scoped>
+    
+</style>
