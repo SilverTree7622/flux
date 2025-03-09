daeb2282ea49bef7d428258579ab5821e51b0071
FluxHunterPortable <dong5966@gmail.com>
2025-03-10 03:56:17 +0900

    [refactor] dynamic main page iframe for showing wip process

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,35 @@
 <template>
-    <div>
-    </div>
+    <iframe
+        v-if="!!opt.fullSrc"
+        ref="$frame"
+        class="w-full h-[100vh] overflow-hidden"
+        :src="opt.fullSrc"  
+    >
+    </iframe>
 </template>
 
 <script setup lang="ts">
+const route = useRoute();
+
+const opt = reactive({
+    fullSrc: '',
+});
+
+const getFullSrc = () => {
+    const tab = route.query['tab'] as string ?? '';
+    const game = route.query['game'] as string ?? '';
+    const src = `outsourcing/${tab}/${game}/index.html`;
+    console.log('src: ', src);
+    return src;
+};
+
 onMounted(async () => {
     await nextTick();
-    navigateTo('/showcase');
+    opt.fullSrc = getFullSrc();
+    if (!opt.fullSrc) {
+        navigateTo('/showcase');
+    }
+
 });
 </script>
 
