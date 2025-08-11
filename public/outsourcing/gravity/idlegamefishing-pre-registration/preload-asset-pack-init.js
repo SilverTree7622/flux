26f176ba2906d38497601f53638f51cd1790d79d
FluxHunterPortable <dong5966@gmail.com>
2025-08-11 19:43:54 +0900

    gravity-idlegamefishing-pre-registration 업데이트

---
+++ public/outsourcing/gravity/idlegamefishing-pre-registration/preload-asset-pack-init.js
@@ -0,0 +1,4 @@
+window.__PRELOAD_ASSET_PACK__ = { 
+                        meta: {"app":"Phaser Editor 2D - Asset Pack Editor","contentType":"phasereditor2d.pack.core.AssetContentType","url":"","version":2,"showAllFilesInBlocks":false,"visible":true}, 
+                        section1: { files: [] }
+                    };
