3414f91ce02cb31a1aaae276f55d9c215e221c1d
FluxHunterPortable <dong5966@gmail.com>
2025-09-02 18:14:32 +0900

    Updates

--- playablead/prod/ragnarok-idle-game-play-version/preload-asset-pack-init.js
+++
@@ -1,4 +0,0 @@
-window.__PRELOAD_ASSET_PACK__ = { 
-                        meta: {"app":"Phaser Editor 2D - Asset Pack Editor","contentType":"phasereditor2d.pack.core.AssetContentType","url":"","version":2,"showAllFilesInBlocks":false,"visible":true}, 
-                        section1: { files: [] }
-                    };
